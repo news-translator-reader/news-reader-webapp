5e3273446692e436c4e5fb559e34cd1ba2a74be5
@kisrakap <kisrakap@gmail.com>
2020-07-10 09:48:35 +0800

    test

--- server/routers/apiRouter.js
+++ server/routers/apiRouter.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
 const ApiController = require('../controllers/ApiController')
+const authentication = require('../middleware/authentication')
 
+router.use(authentication)
 router.get('/news', ApiController.getNews) // methodnya nanti diganti sesuai fungsi api
 router.get('/translate', ApiController.translate) // methodnya nanti diganti sesuai fungsi api
 router.get('/read', ApiController.read) // methodnya nanti diganti sesuai fungsi api
-
-module.exports = router;
